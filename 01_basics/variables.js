68d5dea07f3e922533781f1661d338bc40b045b3
nabeelnaseem9 <83338123+nabeelnaseem9@users.noreply.github.com>
2025-11-05 15:22:28 +0000

    learning variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,10 @@
+const accountId = 12345;
+let accountemail = "nabeel@gmail.com";
+var accountPassword = "323232";
+accountCity = "Dubai";
+
+accountemail = "neelo@gmail.com";
+accountPassword = "121212";
+accountCity = "Sharjah";
+
+console.table([accountId, accountemail, accountPassword, accountCity]);
